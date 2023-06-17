1ae7b4941a894cc9a6ab6492d8af8d795c971edf
kavi-ivykids <136069340+kavi-ivykids@users.noreply.github.com>
2023-06-18 00:18:18 +0530

    make a dummy e commerce list

---
+++ lib/data/storage/local/products.dart
@@ -0,0 +1,68 @@
+class Products {
+  static List<Map<String, dynamic>> productsList = [
+    {
+      'id': 1,
+      'name': 'Product 1',
+      'description': 'This is the description of Product 1.',
+      'price': 9.99,
+      'image':
+          'https://images.meesho.com/images/products/103713364/0ioi6_512.jpg',
+    },
+    {
+      'id': 2,
+      'name': 'Product 2',
+      'description': 'This is the description of Product 2.',
+      'price': 9.99,
+      'image':
+          'https://images.meesho.com/images/products/103713364/0ioi6_512.jpg',
+    },
+    {
+      'id': 3,
+      'name': 'Product 3',
+      'description': 'This is the description of Product 3.',
+      'price': 9.99,
+      'image':
+          'https://images.meesho.com/images/products/103713364/0ioi6_512.jpg',
+    },
+    {
+      'id': 4,
+      'name': 'Product 4',
+      'description': 'This is the description of Product 4.',
+      'price': 9.99,
+      'image':
+          'https://images.meesho.com/images/products/103713364/0ioi6_512.jpg',
+    },
+    {
+      'id': 5,
+      'name': 'Product 5',
+      'description': 'This is the description of Product 5.',
+      'price': 9.99,
+      'image':
+          'https://images.meesho.com/images/products/103713364/0ioi6_512.jpg',
+    },
+    {
+      'id': 6,
+      'name': 'Product 6',
+      'description': 'This is the description of Product 6.',
+      'price': 9.99,
+      'image':
+          'https://images.meesho.com/images/products/103713364/0ioi6_512.jpg',
+    },
+    {
+      'id': 7,
+      'name': 'Product 7',
+      'description': 'This is the description of Product 7.',
+      'price': 9.99,
+      'image':
+          'https://images.meesho.com/images/products/103713364/0ioi6_512.jpg',
+    },
+    {
+      'id': 8,
+      'name': 'Product 8',
+      'description': 'This is the description of Product 8.',
+      'price': 9.99,
+      'image':
+          'https://images.meesho.com/images/products/103713364/0ioi6_512.jpg',
+    },
+  ];
+}
